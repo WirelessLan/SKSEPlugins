839c9569f2716b08133081e07b4ebd46d4bf0b9f
WirelessLan <ywk0716@gmail.com>
2022-06-15 01:37:17 +0900

    Add files via upload

---
+++ SKEE64MemoryLeakPatcher/Global.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <skse64/PluginAPI.h>
+
+#include <skse64_common/skse_version.h>
+#include <skse64_common/SafeWrite.h>
+
+#define PLUGIN_NAME		"SKEE64MemoryLeakPatcher"
+#define PLUGIN_VERSION	MAKE_EXE_VERSION(0, 0, 1)
