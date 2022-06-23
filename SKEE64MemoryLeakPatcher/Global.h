0047d3989c35b0cc004f92a8706646f09670f31b
WirelessLan <ywk0716@gmail.com>
2022-06-23 09:03:50 +0900

    Delete SKEE64MemoryLeakPatcher directory

--- SKEE64MemoryLeakPatcher/Global.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <skse64/PluginAPI.h>
-
-#include <skse64_common/skse_version.h>
-#include <skse64_common/SafeWrite.h>
-
-#define PLUGIN_NAME		"SKEE64MemoryLeakPatcher"
-#define PLUGIN_VERSION	MAKE_EXE_VERSION(0, 0, 1)
