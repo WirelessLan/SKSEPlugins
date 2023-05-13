cb7832e7b574cc49e8fcfa374e03d3f18c58070f
WirelessLan <ywk0716@gmail.com>
2023-05-13 14:25:23 +0900

    Version 0.7.2
    - 내부적 액터/종족 메쉬 순서 처리방식 변경

--- CustomMeshesPathSSE/Global.h
+++ CustomMeshesPathSSE/Global.h
@@ -3,4 +3,4 @@
 #include <skse64_common/skse_version.h>
 
 #define PLUGIN_NAME	"CustomMeshesPathSSE"
-#define PLUGIN_VERSION	MAKE_EXE_VERSION(0, 7, 1)
+#define PLUGIN_VERSION	MAKE_EXE_VERSION(0, 7, 2)
